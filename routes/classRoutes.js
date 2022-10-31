00cd367e5aeaa9559fbbb8dc027671281948fc7b
Alejandra Morales <alejandramorales.2001@unitec.edu>
2022-10-30 21:26:49 -0600

    ADD

---
+++ routes/classRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const classController = require("../controllers/classesController");
+
+// enlace de routes
+router.get("/", classController.getClasses);
+router.get("/:id", classController.getClassById);
+router.delete("/:id", classController.deleteClassById);
+router.put("/:id", classController.updateClass);
+router.post("/", classController.createClass);
+
+module.exports = router;
--- services/students.js
+++ services/students.js
@@ -1,45 +1,83 @@
-const studentsReturn = students.slice();
-students.map(async (student, index) => {
-  students[index].classes = JSON.parse(
+const knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+});
+
+async function getStudents() {
+  const students = JSON.parse(
+    JSON.stringify(await knex.select().table("students"))
+  );
+  const studentsReturn = students.slice();
+  for (let i = 0; i < students.length; i++) {
+    const student = students[i];
+    const classes = await getEnrolledClasses(student.id);
+    studentsReturn[i].classes = classes;
+  }
+  return studentsReturn;
+}
+
+async function getEnrolledClasses(studentId) {
+  const classes = [];
+  const studentsClasses = JSON.parse(
     JSON.stringify(
       await knex
         .select()
         .table("students_classes")
-        .where("student_id", students.id)
+        .where("student_id", studentId)
     )
   );
-  students[index].classes.forEach(async (e) => {
-    const classes = JSON.parse(
-      JSON.stringify(
-        await knex.select().table("classes").where("id", e.classes_id)
-      )
-    );
-    console.log(classes);
-    studentsReturn[index].classes = classes;
-  });
-});
-return studentsReturn;
 
-async function getStudents() {
-  const studentsReturn = students.slice();
-  students.map(async (student, index) => {
-    students[index].classes = JSON.parse(
+  for (let i = 0; i < studentsClasses.length; i++) {
+    const enrolledClass = JSON.parse(
       JSON.stringify(
         await knex
           .select()
-          .table("students_classes")
-          .where("student_id", students.id)
+          .table("classes")
+          .where("id", studentsClasses[i].class_id)
       )
     );
-    students[index].classes.forEach(async (e) => {
-      const classes = JSON.parse(
-        JSON.stringify(
-          await knex.select().table("classes").where("id", e.classes_id)
-        )
-      );
-      console.log(classes);
-      studentsReturn[index].classes = classes;
-    });
-  });
-  return studentsReturn;
+    if (enrolledClass.length) classes.push(enrolledClass[0].name);
+  }
+  return classes;
 }
+
+async function createStudent(student) {
+  return knex("students").insert({
+    name: student.name,
+    age: student.age ? student.age : null,
+  });
+}
+
+async function getStudentById(id) {
+  const student = JSON.parse(
+    JSON.stringify(await knex.select().table("students").where("id", id))
+  );
+  return student;
+}
+
+async function deleteStudentById(id) {
+  return await knex("students").where("id", id).del();
+}
+
+async function updateStudent(id, student) {
+  await knex("students").where("id", "=", id).update({
+    name: student.name,
+    age: student.age,
+  });
+  return;
+}
+
+// exposure to outside
+module.exports = {
+  getStudents,
+  getStudentById,
+  deleteStudentById,
+  createStudent,
+  updateStudent,
+};
